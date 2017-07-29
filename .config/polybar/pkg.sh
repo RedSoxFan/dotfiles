c03a12140534512a2531fd9374462e83178ad3ac
Brian Ashworth <bosrsf04@gmail.com>
2017-07-28 20:34:12 -0400

    polybar: Changed pkg script to output 0/0 when up-to-date so that the bar gets updated

--- .config/polybar/pkg.sh
+++ .config/polybar/pkg.sh
@@ -3,7 +3,4 @@ pac=$(checkupdates | wc -l)
 aur=$(cower -u | wc -l)
 
 check=$((pac + aur))
-if [[ "$check" != "0" ]]
-then
-    echo "$pac %{F#5b5b5b}%{F-} $aur"
-fi
+echo "$pac %{F#5b5b5b}%{F-} $aur"
