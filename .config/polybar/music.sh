6148764a2c17ffbb10793b0b9a4dd5195ceecddb
Brian Ashworth <bosrsf04@gmail.com>
2017-09-11 22:01:00 -0400

    polybar: Moves music to a script (w/ ad detect)

---
+++ .config/polybar/music.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+state=$(playerctl status 2>/dev/null | grep -E "(Playing|Paused)" | sed "s/Playing//" | sed "s/Paused//")
+
+artist=$(playerctl metadata 'xesam:artist' 2> /dev/null)
+title=$(playerctl metadata 'xesam:title' 2> /dev/null)
+ad=$(playerctl metadata 'mpris:trackid' 2> /dev/null | grep -E '^spotify:ad:')
+
+if [ -z $ad ]; then
+	echo $state" "$artist" - "$title | sed -r 's/^  - $//'
+else
+	echo $state" Advertisement ("$title")"
+fi
