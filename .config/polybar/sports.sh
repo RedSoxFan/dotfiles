a621a5a1ae3153704f6aac321e4bdfb4bc3d5c7b
Brian Ashworth <bosrsf04@gmail.com>
2018-02-06 14:48:11 -0500

    polybar: disables sports module for off-seasons
    polybar/sports: prepares script for mlb season
    nfl: fixes post season game polling

--- .config/polybar/sports.sh
+++ .config/polybar/sports.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
+SOX=$(~/.local/share/bin/mlb 2>/dev/null)
 # Commented out for the offseason
-# SOX=$(~/.local/share/bin/mlb 2>/dev/null)
-SOX=""
-PATS=$(~/.local/share/bin/nfl --today 2>/dev/null)
+# PATS=$(~/.local/share/bin/nfl --today 2>/dev/null)
+PATS=""
 
 echo -e $SOX"         "$PATS | sed -r "s/^\s+|\s+$//g"
